37284e3f5339cea6530af5055749cf32e8fc9ffc
李莹莹 <liyingying@liyingyingdeMacBook-Pro.local>
2019-05-25 23:01:48 +0800

    react 第一天学习

---
+++ src/TodoList.js
@@ -0,0 +1,82 @@
+import React,{Component,Fragment} from 'react'
+import './style.css'
+import TodoItem from './Todoitem'
+
+class TodoList extends Component {
+
+constructor(props){
+    super(props);
+    this.state = {
+        inputValue:'hello!!',
+        list:[]
+    }
+    this.handleInputChange = this.handleInputChange.bind(this)
+    this.handleBtnClick=this.handleBtnClick.bind(this)
+    this.handleItemDelete=this.handleItemDelete.bind(this)
+}
+   render(){
+        return(
+            <Fragment>
+            
+                <div>
+                    <label htmlFor='insertArea'>输入内容</label>
+                <input 
+                id='insertArea'
+                className='input'
+                value={this.state.inputValue}
+                onChange = {this.handleInputChange}
+                />
+                <button onClick={this.handleBtnClick}>提交</button></div>
+            <ul>
+             {this.getTodoItem()}
+            </ul>
+            </Fragment>
+        )
+    }
+    getTodoItem(){
+      return this.state.list.map((item,index)=>{
+    return (  
+    <TodoItem 
+    key={index}
+    content={item} 
+    index={index}
+    deleteItem={this.handleItemDelete}/>
+    // <li key={index}
+    //  onClick={this.handleItemDelete.bind(this,index)}
+    //  dangerouslySetInnerHTML ={{__html:item}}
+    //  ></li>
+    
+    )
+           
+        })
+    }
+    handleInputChange(e){
+        const value = e.target.value
+    this.setState(()=>({
+        inputValue: value
+    }))
+    }
+    handleBtnClick(e){
+        this.setState((prevState)=>({
+            list:[...prevState.list,prevState.inputValue],
+            inputValue:""
+        }))
+
+    }
+    handleItemDelete(index){
+        
+        this.setState((prevState)=>{
+            const list = [...prevState.list];
+            list.splice(index,1);
+            return {
+                list
+            }
+        })
+       
+    }
+
+}
+
+
+
+export default TodoList
